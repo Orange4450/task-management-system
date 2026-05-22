83ac719f5a034f10174e9cee4acd43a280cb6278
Orange4450 <37318@iitu.edu.kz>
2026-05-23 00:33:08 +0500

    Created Config AsyncConfig

---
+++ src/main/java/com/Boltabay_Zhaniya/taskmanagement/config/Boltabay_Zhaniya_AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.Boltabay_Zhaniya.taskmanagement.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class Boltabay_Zhaniya_AsyncConfig {
+}
