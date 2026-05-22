cf0e1b971f6ea4752c89b06ace1eb926b19f4efa
Orange4450 <37318@iitu.edu.kz>
2026-05-22 23:30:41 +0500

    Created Exception ResourceNotFound

---
+++ src/main/java/com/Boltabay_Zhaniya/taskmanagement/exception/Boltabay_Zhaniya_ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.Boltabay_Zhaniya.taskmanagement.exception;
+
+public class Boltabay_Zhaniya_ResourceNotFoundException  extends RuntimeException {
+    public Boltabay_Zhaniya_ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
