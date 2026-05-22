e6a9ddd6078396d4535464fa6774ef8a016bfb1c
Orange4450 <37318@iitu.edu.kz>
2026-05-22 23:15:41 +0500

    Created DTO AuthResponse

---
+++ src/main/java/com/Boltabay_Zhaniya/taskmanagement/dto/Boltabay_Zhaniya_AuthResponseDto.java
@@ -0,0 +1,10 @@
+package com.Boltabay_Zhaniya.taskmanagement.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Boltabay_Zhaniya_AuthResponseDto {
+    private String token;
+}
