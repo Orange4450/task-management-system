34441ab531844ac0c19f6a4e1ea292cd0ca8f9f5
Orange4450 <37318@iitu.edu.kz>
2026-05-22 23:51:45 +0500

    Created Security JwtUtil

---
+++ src/main/java/com/Boltabay_Zhaniya/taskmanagement/security/Boltabay_Zhaniya_JwtUtil.java
@@ -0,0 +1,40 @@
+package com.Boltabay_Zhaniya.taskmanagement.security;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class Boltabay_Zhaniya_JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    public String generateToken(String email) {
+
+        Key key = Keys.hmacShaKeyFor(secret.getBytes());
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+
+        return Jwts.parserBuilder()
+                .setSigningKey(secret.getBytes())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
