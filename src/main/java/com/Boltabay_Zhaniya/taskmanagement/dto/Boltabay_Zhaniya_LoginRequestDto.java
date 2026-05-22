47af21eb6115e59081e47a67248acfdb5be1983c
Orange4450 <37318@iitu.edu.kz>
2026-05-22 23:13:40 +0500

    Created DTO LoginRequest

---
+++ src/main/java/com/Boltabay_Zhaniya/taskmanagement/dto/Boltabay_Zhaniya_LoginRequestDto.java
@@ -0,0 +1,8 @@
+package com.Boltabay_Zhaniya.taskmanagement.dto;
+import lombok.Data;
+
+@Data
+public class Boltabay_Zhaniya_LoginRequestDto {
+    private String email;
+    private String password;
+}
