5d080326bfd53bfac33dd8c13ba2fb114a1a65d1
Orange4450 <37318@iitu.edu.kz>
2026-05-23 01:41:28 +0500

    Adding Dockerfile and docker-compose

--- src/main/java/com/Boltabay_Zhaniya/taskmanagement/security/Boltabay_Zhaniya_SecurityConfig.java
+++ src/main/java/com/Boltabay_Zhaniya/taskmanagement/security/Boltabay_Zhaniya_SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -28,11 +29,12 @@ public class Boltabay_Zhaniya_SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/api/auth/**",
                                 "/swagger-ui/**",
+                                "/swagger-ui.html",
                                 "/v3/api-docs/**",
                                 "/actuator/**"
                         )
