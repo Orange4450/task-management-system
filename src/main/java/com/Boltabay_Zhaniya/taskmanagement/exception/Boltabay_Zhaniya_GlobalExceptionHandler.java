1fa1d9742efcbb4080cac8a61b04163f3a54d76c
Orange4450 <37318@iitu.edu.kz>
2026-05-22 23:48:32 +0500

    Created Exception GlobalExceptionHandler

---
+++ src/main/java/com/Boltabay_Zhaniya/taskmanagement/exception/Boltabay_Zhaniya_GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.Boltabay_Zhaniya.taskmanagement.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class Boltabay_Zhaniya_GlobalExceptionHandler {
+
+    @ExceptionHandler(Boltabay_Zhaniya_ResourceNotFoundException.class)
+    public ResponseEntity<?> handleNotFound(Boltabay_Zhaniya_ResourceNotFoundException ex) {
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidation(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors()
+                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
